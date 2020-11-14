2185c1a6999dfbe2fe22403e5db4cc1e748840f8
frinzekt <frinzekt@gmail.com>
2020-11-14 13:23:39 +0800

    animation

--- vue/sim-app/src/components/GraphSim.vue
+++ vue/sim-app/src/components/GraphSim.vue
@@ -48,7 +48,29 @@ export default {
   },
   watch: {
     caseData() {
-      Plotly.react(this.graphElement, [this.caseData], this.layout, this.config);
+      const xAxis = [...this.caseData.x];
+      const yAxis = [...this.caseData.y];
+      Plotly.animate(this.graphElement, {
+        data: [this.caseData],
+        traces: [0],
+        layout: {
+          xaxis: { range: [Math.min(...xAxis), Math.max(...xAxis)] },
+          yaxis: { range: [Math.min(...yAxis), Math.max(...yAxis)] },
+          margin: {
+            t: 50,
+            b: 50,
+          },
+        },
+      },
+      {
+        transition: {
+          duration: 500,
+          easing: 'cubic-in-out',
+        },
+        frame: {
+          duration: 500,
+        },
+      });
     },
   },
 };
