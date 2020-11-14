a5ff815d7d8803f9cb1d3622e02f773d3598cb90
Max Matthews <21506225@student.uwa.edu.au>
2020-11-14 17:04:02 +0800

    bawdyy awvv theee raaaaayytt

--- vue/sim-app/src/views/Maps.vue
+++ vue/sim-app/src/views/Maps.vue
@@ -1,8 +1,41 @@
 <template>
-        <div class="chartdiv" ref="chartdiv" id="chartdiv" style="width: 100%;height:100%">
+  <div class="chartdiv" ref="chartdiv" id="chartdiv" style="width: 100%; height:100%">
+      <v-row justify="center">
+    <v-dialog
+      v-model="dialog"
+      persistent
+      max-width="70%"
+    >
+      <v-card>
+        <v-card-title class="headline">
+          Simulate COVID-19 mitigation strategies for:
+           <span class="ml-3 "><b><u>{{selectedCountry}}</u></b></span>
+        </v-card-title>
+        <v-card-text>Let Google help apps determine location.
+          This means sending anonymous location data to Google, even when no apps are running.
+          </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="green darken-1"
+            text
+            @click='zoomOutBro'
+          >
+            Choose another
+          </v-btn>
+          <v-btn
+            color="green darken-1"
+            text
+            @click="dialog = false"
+          >
+            Proceed with {{selectedCountry}}
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
   </div>
 </template>
-
 <script>
 import * as am4core from '@amcharts/amcharts4/core';
 import * as am4maps from '@amcharts/amcharts4/maps';
@@ -14,12 +47,23 @@ am4core.useTheme(am4themes_animated);
 export default {
   components: { },
   data: () => ({
+    dialog: false,
+    selectedCountry: '',
   }),
+  methods: {
+    zoomOutBro() {
+      this.dialog = false;
+      this.selectedCountry = '';
+      window.chart.goHome();
+    },
+  },
   mounted() {
     // Create map instance
     const chart = am4core.create('chartdiv', am4maps.MapChart);
+    window.chart = chart;
     chart.width = am4core.percent(100);
     chart.height = am4core.percent(100);
+    chart.chartContainer.wheelable = false;
 
     // Set map definition
     chart.geodata = am4geodata_worldLow;
@@ -45,7 +89,8 @@ export default {
     polygonTemplate.events.on('hit', (ev) => {
       // zoom to an object
       ev.target.series.chart.zoomToMapObject(ev.target);
-
+      this.selectedCountry = ev.target.dataItem.dataContext.name;
+      this.dialog = !this.dialog;
       // get object info
       console.log(ev.target.dataItem.dataContext.name);
     });
