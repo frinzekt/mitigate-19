484b4ecc10bf90d4bc0b2a79e69ed17f1c2e1d20
Max Matthews <21506225@student.uwa.edu.au>
2020-11-14 23:38:18 +0800

    big big scary rodent kinda looks like maybe a.... RAT

--- vue/sim-app/src/pandemic/alert.js
+++
@@ -1,9 +0,0 @@
-/**
- * Displays an alert if the user is not using Chrome, since
- * it is the only fully supported browser.
- */
-function alertIfNotChrome() {
-  if (navigator.userAgent.indexOf("Chrome") === -1) {
-    alert("This page works best on Chrome.\nIf you're having problems using this page, try to open it in Chrome.");
-  }
-}
