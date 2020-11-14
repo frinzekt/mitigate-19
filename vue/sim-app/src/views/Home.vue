d3628c06b84dcd9548ae78476f4dc3dc0a8fa87d
frinzekt <frinzekt@gmail.com>
2020-11-14 21:23:59 +0800

    RELAYOUT

--- vue/sim-app/src/views/Home.vue
+++ vue/sim-app/src/views/Home.vue
@@ -1,13 +1,17 @@
 <template>
   <div class='home'>
     <v-container>
-      <v-row
-      wrap
-      >
-        <v-col cols='6'>
-          <SimControls />
-        </v-col>
-        <v-col cols='6'>
+      <v-row align="center"
+      justify="center"
+        wrap
+        >
+          <v-col cols='6'>
+            <SimStatistics />
+          </v-col>
+        </v-row>
+    </v-container>
+    <v-row wrap>
+        <v-col cols='4'>
             <v-card>
               <totalCasesSim
               :graphElement='totalCasesDiv'
@@ -15,16 +19,31 @@
               <div :id='totalCasesDiv' class='graph'>
               </div>
             </v-card>
-          <br/>
-             <v-card>
-              <newCasesSim
-              :graphElement='newCasesDiv'
-              />
-              <div :id='newCasesDiv' class='graph'>
-              </div>
-            </v-card>
+        </v-col>
+        <v-col cols='4'>
+          <v-card>
+          <newCasesSim
+          :graphElement='newCasesDiv'
+          />
+          <div :id='newCasesDiv' class='graph'>
+          </div>
+        </v-card>
+        </v-col>
+        <v-col cols='4'>
+          <v-card>
+         Third Graph here
+        </v-card>
         </v-col>
       </v-row>
+    <v-container>
+      <v-row align="center"
+      justify="center"
+        wrap
+        >
+          <v-col cols='12'>
+            <SimControls></SimControls>
+          </v-col>
+        </v-row>
     </v-container>
   </div>
 </template>
@@ -32,6 +51,7 @@
 <script>
 import totalCasesSim from '../components/totalCasesSim.vue';
 import newCasesSim from '../components/newCasesSim.vue';
+import SimStatistics from '../components/SimStatistics.vue';
 import SimControls from '../components/SimControls.vue';
 
 // @ is an alias to /src
@@ -47,6 +67,7 @@ export default {
     totalCasesSim,
     newCasesSim,
     SimControls,
+    SimStatistics,
   },
   mounted() {
     this.graphElem = document.getElementById('graphElem');
