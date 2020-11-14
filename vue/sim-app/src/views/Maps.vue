9f108a64936b431c195a8dc1cbb0b774ef60dc03
frinzekt <frinzekt@gmail.com>
2020-11-14 14:40:44 +0800

    vue map

---
+++ vue/sim-app/src/views/Maps.vue
@@ -0,0 +1,20 @@
+<template>
+      <gmaps-map>
+        <gmaps-info-window v-for="(item, i) in items" :key="i" :options="item.options">
+            <p class="black--text">{{item.countryName}}</p>
+            <v-btn>Click</v-btn>
+        </gmaps-info-window>
+      </gmaps-map>
+</template>
+
+<script>
+import { gmapsMap, gmapsInfoWindow } from 'x5-gmaps';
+import mapsData from '../data/maps';
+
+export default {
+  components: { gmapsMap, gmapsInfoWindow },
+  data: () => ({
+    items: mapsData,
+  }),
+};
+</script>
